041384a2b6650a168cd59d7cfd86bc55d09314e9
ashkanmohseni <ashkanmohseni@macbook-pro>
2020-03-16 16:42:19 -0400

    Fixed png logo and added font

--- src/fx/main.java
+++ src/fx/main.java
@@ -1,6 +1,7 @@
 package fx;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -14,6 +15,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 /**
@@ -36,28 +39,34 @@ public class main extends Application {
     public void start(Stage primaryStage) {
 
         window = primaryStage;
-
+        
+        // added font
         Label welcome = new Label("Welcome to the NHL Stat System");
+        welcome.setFont(Font.font("noteworthy", FontWeight.BOLD, 35));
 
         //First button
         Button loginBtn = new Button();
         loginBtn.setText("Submit");
+        loginBtn.setFont(Font.font("Gill Sans", FontWeight.NORMAL, 20));
         loginBtn.setOnAction(e -> window.setScene(scene2));//switch to scene2 where we can display the scores
 
         //HBox for logo and description
         HBox logoDesc = new HBox();
-        ImageView logo = new ImageView(new Image("images/nhl.jpg"));
+        ImageView logo = new ImageView(new Image("images/nhl.png"));
         logo.setFitHeight(100);
-        logo.setFitWidth(100);
+        logo.setFitWidth(80);
         logoDesc.setAlignment(Pos.CENTER);
         Label description = new Label("Our app does blah blah _____");
+        description.setFont(Font.font("Avenir", FontWeight.NORMAL, 20));
         logoDesc.getChildren().addAll(logo, description);
         
         //instruction label
         Label login = new Label("Please login to be able to change the schedule!");
+        login.setFont(Font.font("Avenir", FontWeight.NORMAL, 20));
         
         //username text field
         TextField userName = new TextField("username");
+        userName.setFont(Font.font("Avenir", FontWeight.NORMAL, 14));
         userName.setMaxWidth(200);
         
         //password text field
@@ -68,7 +77,7 @@ public class main extends Application {
         VBox layout1 = new VBox(15);
         layout1.setAlignment(Pos.CENTER);
         layout1.getChildren().addAll(welcome, logoDesc,login, userName, password, loginBtn);
-        scene1 = new Scene(layout1, 600, 300);
+        scene1 = new Scene(layout1, 600, 375);
 
         //Second button
         Button btn2 = new Button();
@@ -78,7 +87,7 @@ public class main extends Application {
         // Stackpane puts the button in the center
         StackPane layout2 = new StackPane();
         layout2.getChildren().add(btn2);
-        scene2 = new Scene(layout2, 600, 300);
+        scene2 = new Scene(layout2, 600, 375);
 
         //displaying everything
         window.setScene(scene1);
